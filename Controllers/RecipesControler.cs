cf90cbd1d6b2fa03847b59343bcb449cfa3a71fb
welinton-martins <welintonmartins4@outlook.com>
2025-04-02 11:38:09 -0300

    Initial commit

---
+++ Controllers/RecipesControler.cs
@@ -0,0 +1,51 @@
+﻿using ContosoRecipes.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoRecipes.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecipesControler : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult GetRecipes([FromQuery]int count)
+        { 
+            Recipe[] recipes =
+            {
+                new() { Title = "Oxtail" }, 
+                new() { Title = "Curry Chicken" },
+                new() { Title = "Dumplings" }
+            };
+            if (!recipes.Any())
+                return NotFound();
+            return Ok(recipes.Take(count));
+        }
+
+        [HttpPost]
+        public ActionResult CreateNewRecipes([FromBody] Recipe newRecipe)
+        {
+            // validade and save to database
+            bool badThingsHappened = false;
+            if (badThingsHappened)
+                return BadRequest();
+
+            return Created("", newRecipe);
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteRecipes(string id)
+        {
+            bool badThingsHappened = false;
+
+            if (badThingsHappened)
+                return BadRequest();
+            return NoContent();
+        }
+
+    }
+}
---
+++ Models/Records.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoRecipes.Models
+{
+    public class Recipe
+    {
+        public string Title { get; init; }
+        public string Description { get; init; }
+        public IEnumerable<string> Directions { get; init; }
+        public IEnumerable<string> Ingredients { get; init; }
+        public DateTime Updated { get; init; }
+    }
+}
